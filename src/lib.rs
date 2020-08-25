d7b4ba25640e09f906dc1f472cff16f86e873c00
pca006132 <john.lck40@gmail.com>
2020-08-25 14:35:48 +0800

    Removed libm dependency

--- src/lib.rs
+++ src/lib.rs
@@ -41,12 +41,12 @@ mod macros;
 pub mod float;
 pub mod int;
 
-#[cfg(any(
-    all(target_arch = "wasm32", target_os = "unknown"),
-    all(target_arch = "arm", target_os = "none"),
-    all(target_vendor = "fortanix", target_env = "sgx")
-))]
-pub mod math;
+// #[cfg(any(
+//     all(target_arch = "wasm32", target_os = "unknown"),
+//     all(target_arch = "arm", target_os = "none"),
+//     all(target_vendor = "fortanix", target_env = "sgx")
+// ))]
+// pub mod math;
 pub mod mem;
 
 #[cfg(target_arch = "arm")]
